5a5cbc73675a67e8437fd1a6fb00212512302236
Tiago Ribeiro <tribeiro@lsst.org>
2023-11-18 05:57:25 -0300

    Render confluence markdown.

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -101,3 +101,65 @@ export async function fetchNarrativeLogFromId(id: string | undefined) {
 		}		
 	} 
 }
+
+export function jiraMarkdownToHtml(markdown: string, options = { codeFriendly: true, parseLines: true }) {
+	if (!markdown) return '';
+  
+	const { codeFriendly, parseLines } = options;
+	let html = markdown;
+  
+	// Parse text formats
+	if (!codeFriendly) {
+	  html = html.replace(/\*(.*)\*/g, (match, p1) => {
+		return `<strong>${p1}</strong>`;
+	  });
+	  html = html.replace(/_(.*)_/g, (match, p1) => {
+		return `<em>${p1}</em>`;
+	  });
+	  html = html.replace(/\+(.*)\+/g, (match, p1) => {
+		return `<u>${p1}</u>`;
+	  });
+	  html = html.replace(/-(.*)-/g, (match, p1) => {
+		return `<s>${p1}</s>`;
+	  });
+	}
+  
+	// Parse headings
+	html = html.replace(/h1\.\s(.*)/g, (match, p1) => {
+	  return `<h1>${p1}</h1>`;
+	});
+	html = html.replace(/h2\.\s(.*)/g, (match, p1) => {
+	  return `<h2>${p1}</h2>`;
+	});
+	html = html.replace(/h3\.\s(.*)/g, (match, p1) => {
+	  return `<h3>${p1}</h3>`;
+	});
+	html = html.replace(/h4\.\s(.*)/g, (match, p1) => {
+	  return `<h4>${p1}</h4>`;
+	});
+	html = html.replace(/h5\.\s(.*)/g, (match, p1) => {
+	  return `<h5>${p1}</h5>`;
+	});
+	html = html.replace(/h6\.\s(.*)/g, (match, p1) => {
+	  return `<h6>${p1}</h6>`;
+	});
+  
+	// Parse links
+	html = html.replace(/\[(.*?)\|(.*?)\]/g, (match, p1, p2) => {
+	  return `<a href="${p2}" rel="noopener noreferrer" target="_blank">${p1}</a>`;
+	});
+  
+	// Parse code blocks
+	html = html.replace(/\{code\}(.*)\{code\}/g, (match, p1) => {
+	  return `<code>${p1}</code>`;
+	});
+  
+	// Parse full lines
+	if (parseLines) {
+	  html = html.replace(/^(\s*)(.*)\r\n/gm, (match, p1, p2) => {
+		return `<p>${[...p1].map((e) => '&nbsp;').join('')}${p2}</p>`;
+	  });
+	}
+  
+	return html;
+  }
